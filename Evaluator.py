80e3e0187e5ad5d9ae24010c18ba2575c45e9dce
bofan.wang <bofan.wang@graviti.cn>
2021-09-01 17:45:43 +0800

    curve and float for file

--- Evaluator.py
+++ Evaluator.py
@@ -3,16 +3,24 @@ import numpy as np
 class Evaluator:
     def __init__(self):
         self.cnt = 0
+        self.x = np.arange(start=-50, stop=50, step=1)
         """
         You can initialize your model here
         """
         self.result = []
-        ...
+
     def evaluate_one_data(self, input_source: dict, input_target: dict) -> dict:
         self.cnt = self.cnt + 0.5
+        ratio = np.tan(self.cnt)
+        y = self.x * ratio
+
         dict_ret = {
                 'overall': {
                     'float_file_metric': self.cnt,
+                    'curve_file_metric': {
+                        'x': self.x.tolist(),
+                        'y': y.tolist()
+                    }
                 }
         }
         self.result = self.result + [self.cnt]
@@ -21,10 +29,17 @@ class Evaluator:
     def get_result(self) -> dict:
         r = np.array(self.result)
         r_float = float(r.mean())
+        self.cnt = self.cnt + 0.5
+        ratio = np.tan(self.cnt)
+        y = self.x * ratio
         dict_ret = {
             'overall': {
-                'float_file_metric': r_float,
-            }
+                    'float_file_metric': self.cnt,
+                    'curve_file_metric': {
+                        'x': self.x.tolist(),
+                        'y': y.tolist()
+                    }
+                }
         }
         return dict_ret
 
